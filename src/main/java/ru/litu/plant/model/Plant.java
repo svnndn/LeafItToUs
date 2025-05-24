75aa399808af5bf0dfe5c894c4169217e841528f
Kamila Ziganshina <svnndndn@gmail.com>
2025-05-24 03:47:02 +0300

    fix plant, plants mapping, add getter/setter without lombok

--- src/main/java/ru/litu/plant/model/Plant.java
+++ src/main/java/ru/litu/plant/model/Plant.java
@@ -7,8 +7,6 @@ import lombok.*;
 
 @Entity
 @Table(name = "plants", uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
-@Getter
-@Setter
 @ToString
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
@@ -39,4 +37,75 @@ public class Plant {
 
     private String fertilizingFrequency;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public CareLevel getCareLevel() {
+        return careLevel;
+    }
+
+    public void setCareLevel(CareLevel careLevel) {
+        this.careLevel = careLevel;
+    }
+
+    public String getWateringFrequency() {
+        return wateringFrequency;
+    }
+
+    public void setWateringFrequency(String wateringFrequency) {
+        this.wateringFrequency = wateringFrequency;
+    }
+
+    public String getLightingNeeds() {
+        return lightingNeeds;
+    }
+
+    public void setLightingNeeds(String lightingNeeds) {
+        this.lightingNeeds = lightingNeeds;
+    }
+
+    public String getFertilizingFrequency() {
+        return fertilizingFrequency;
+    }
+
+    public void setFertilizingFrequency(String fertilizingFrequency) {
+        this.fertilizingFrequency = fertilizingFrequency;
+    }
 }
